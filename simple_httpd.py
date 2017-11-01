82672e837e6f75ef15b0edd250d4308fba23a351
Zarper <zawnpn@gmail.com>
2017-11-01 19:38:35 +0800

    bug fixes & updates

--- simple_httpd.py
+++ simple_httpd.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-port = 6666
+port = 12345
 
 httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
 print("Starting simple_httpd on port: " + str(httpd.server_port))
